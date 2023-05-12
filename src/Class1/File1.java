7d27722389e4d0da33a229b58ed11a7156513a37
Svetlana18 <ramashka1881@gmail.com>
2023-05-11 21:24:07 -0400

    file1 is created

--- src/Class1/File1.java
+++ src/Class1/File1.java
@@ -1,2 +1,14 @@
-package Class1;public class File1 {
+package Class1;
+
+public class File1 {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello word");
+
+
+
+    }
 }
